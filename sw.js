cf0272def0d088692ba0e3a2cb3590842d045baa
dadosturismo <hakuhimanashi@gmail.com>
2026-09-11 23:01:46 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'atendimentos-pwa-v1';
+const ARQUIVOS = ['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARQUIVOS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => { if (e.request.method === 'GET' && new URL(e.request.url).origin === location.origin) e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
